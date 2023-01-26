f4426ab2390a750eadda11794a58fba8175bde8e
Adrin T Paul <109350438+themechbro@users.noreply.github.com>
2023-01-26 14:48:59 +0530

    Update indexpageMap.js

--- public/javascripts/indexpageMap.js
+++ public/javascripts/indexpageMap.js
@@ -2,7 +2,7 @@
 const map = new mapboxgl.Map({
 container: 'cluster-map',// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/dark-v11',
-center: [-103.5917, 40.6699],
+center: [76.6469, 8.939379],
 zoom: 3
 });
  
